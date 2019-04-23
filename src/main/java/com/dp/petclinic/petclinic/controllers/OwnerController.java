23e476b084b10efba70afb25b42f40814f521a47
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-24 00:26:16 +0300

    Create OwnerController.java

---
+++ src/main/java/com/dp/petclinic/petclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package com.dp.petclinic.petclinic.controllers;
+
+import com.dp.petclinic.petclinic.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/owners")
+public class OwnerController {
+
+    @Autowired
+    OwnerService ownerService;
+
+    
+}
