db3beb5272499b7046021de1b839c04a8d4d4a2e
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-23 23:46:15 +0300

    added Vet model

---
+++ src/main/java/com/dp/petclinic/petclinic/models/Vet.java
@@ -0,0 +1,91 @@
+package com.dp.petclinic.petclinic.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "vet")
+public class Vet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "full_name")
+    private String fullName;
+
+    @NotNull
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+    @NotNull
+    @Column(name = "telephone")
+    private String telephone;
+
+    @NotNull
+    @Column(name = "specialization")
+    private String specialization;
+
+    public Vet(String fullName, String email, String password, String telephone, String specialization) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.telephone = telephone;
+        this.specialization = specialization;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+}
