447ad293bc0b2660d15440bf5f417861ee222b20
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-23 23:29:05 +0300

    added Person entity
    which created person table

---
+++ src/main/java/com/dp/petclinic/petclinic/models/Person.java
@@ -0,0 +1,78 @@
+package com.dp.petclinic.petclinic.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "full_name")
+    private String fullName;
+
+    @NotNull
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+    @NotNull
+    @Column(name = "telephone")
+    private String telephone;
+
+    public Person(String fullName, String email, String password, String telephone) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.telephone = telephone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
