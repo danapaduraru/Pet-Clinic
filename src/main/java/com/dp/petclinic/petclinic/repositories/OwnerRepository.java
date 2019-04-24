b81807bccd415aa0f77a3d7c4b1d881aa7d7939d
Dana Paduraru <38670077+danapaduraru@users.noreply.github.com>
2019-04-24 23:31:47 +0300

    deleted Owner

--- src/main/java/com/dp/petclinic/petclinic/repositories/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dp.petclinic.petclinic.repositories;
-
-import com.dp.petclinic.petclinic.models.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-}
